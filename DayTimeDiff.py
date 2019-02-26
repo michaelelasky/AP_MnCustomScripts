7a4ea21805c49d6756563ef46e60a473247c6ce2
Mike Elasky <michael.elasky@state.mn.us>
2019-02-26 11:49:30 -0600

    added check for correct time format

--- DayTimeDiff.py
+++ DayTimeDiff.py
@@ -1,11 +1,19 @@
 # Author(s) Michael Elasky
-# Date 02/08/19
+# Date 02/26/19
 # Title DayTimeDiff
 # Combines 2, day-time objects into datetime and calculates the duration between them then returns a day number,
 # break message and day tolerance.
-# Code version 5.00
+# Code version 6.00
 # Type (Python Custom Module)
+import re
+import time
 
+time_re = re.compile(r'^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$')
+
+def is_time_format(s):
+	tm = bool(time_re.match(s))
+
+	return tm
 
 def combinedatetime(isdatei = None,istimei = None):
 	import clr
@@ -19,6 +27,8 @@ def daysbetweenbreaks(mixdatei = None,mixtimei = None,breakdatei = None,breaktim
 	from System import DateTime
 	from System import TimeSpan
 	from System import Convert
+	import time
+	import re
 	
 	daywithintolerance = False
 	day = None
@@ -26,91 +36,96 @@ def daysbetweenbreaks(mixdatei = None,mixtimei = None,breakdatei = None,breaktim
 	break_message= 'Outside of time requirements '
 	
 	if mixdatei and mixtimei and breakdatei and breaktimei:
-		mixdt=combinedatetime(mixdatei,mixtimei)
-		breakdt=combinedatetime(breakdatei,breaktimei)
-		if mixdt <= DateTime.Now and breakdt >= mixdt:
-			dateduration = Convert.ToDateTime(breakdatei) - Convert.ToDateTime(mixdatei)
-			dt = dateduration.Days
-			duration = breakdt - mixdt
-			durations = duration.TotalSeconds
-			day = duration
-			d=duration.Days
-			h=duration.Hours
-			m=duration.Minutes
+		mt = is_time_format(mixtimei)
+		bt = is_time_format(breaktimei)
+		
+		if mt and bt is True:
+			mixdt=combinedatetime(mixdatei,mixtimei)
+			breakdt=combinedatetime(breakdatei,breaktimei)
+		
+			if mixdt <= DateTime.Now and breakdt >= mixdt:
+				dateduration = Convert.ToDateTime(breakdatei) - Convert.ToDateTime(mixdatei)
+				dt = dateduration.Days
+				duration = breakdt - mixdt
+				durations = duration.TotalSeconds
+				day = duration
+				d=duration.Days
+				h=duration.Hours
+				m=duration.Minutes
 			
-			SECONDS_PER_MINUTE  = 60
-			SECONDS_PER_HOUR    = 3600
-			SECONDS_PER_DAY     = 86400
-			dur = 0
+				SECONDS_PER_MINUTE  = 60
+				SECONDS_PER_HOUR    = 3600
+				SECONDS_PER_DAY     = 86400
+				dur = 0
 	
-			duration_in_sec = duration.TotalSeconds    # Total number of seconds between dates
-			#84600-88200 24hr+-.5hr,255600-262800 d3+-1hr,594000-615600 d7+-3hrs,2376000-2462400 d28+-12hrs,4752000-4924800 d56+-24hrs
-			seconds_range=[(84599, 88201),(255599, 262801),(593999,615601),(2375999, 2462401),(4751999, 4924801)]
+				duration_in_sec = duration.TotalSeconds    # Total number of seconds between dates
+				#84600-88200 24hr+-.5hr,255600-262800 d3+-1hr,594000-615600 d7+-3hrs,2376000-2462400 d28+-12hrs,4752000-4924800 d56+-24hrs
+				seconds_range=[(84599, 88201),(255599, 262801),(593999,615601),(2375999, 2462401),(4751999, 4924801)]
 			
-			for sr in seconds_range:
-				for r in sr:
-					if duration_in_sec in range(sr[0],sr[1]):
-						daywithintolerance = True
-						break_message = 'Inside of time requirements'
-						day=duration.Days
-						if day == 0 or day == 2 or day == 6 or day == 27 or day == 55:
-							day = day + 1
-						elif day == 8 or day == 29 or day == 57:
-							day = day - 1
-						else:
-							day = day
+				for sr in seconds_range:
+					for r in sr:
+						if duration_in_sec in range(sr[0],sr[1]):
+							daywithintolerance = True
+							break_message = 'Inside of time requirements'
+							day=duration.Days
+							if day == 0 or day == 2 or day == 6 or day == 27 or day == 55:
+								day = day + 1
+							elif day == 8 or day == 29 or day == 57:
+								day = day - 1
+							else:
+								day = day
 							
-			if d <= 1 and dt <= 2:
-				if durations < 84600:
-					dur = 84600 - durations
-				elif durations > 88200:
-					dur = durations - 88200
-			elif d >= 2 and dt <= 3 or dt == 4:
-				if durations < 255600:
-					dur = 255600 - durations
-				elif durations > 262800:
-					dur = durations - 262800
-			elif d >= 6 and dt <= 7 or dt == 8:
-				if durations < 594000:
-					dur = 594000 - durations
-				elif durations > 615600:
-					dur = durations - 615600
-			elif d >= 27 and dt == 28 or dt == 29:
-				if durations < 2376000:
-					dur = 2376000 - durations
-				elif durations > 2462400 :
-					dur = durations - 2462400 
-			elif d >= 55 and dt == 56 or dt == 57:
-				if durations < 4752000:
-					dur = 4752000 - durations
-				elif durations > 4924800:
-					dur = durations - 4924800
-			else:
-				dur = durations
+				if d <= 1 and dt <= 2:
+					if durations < 84600:
+						dur = 84600 - durations
+					elif durations > 88200:
+						dur = durations - 88200
+				elif d >= 2 and dt <= 3 or dt == 4:
+					if durations < 255600:
+						dur = 255600 - durations
+					elif durations > 262800:
+						dur = durations - 262800
+				elif d >= 6 and dt <= 7 or dt == 8:
+					if durations < 594000:
+						dur = 594000 - durations
+					elif durations > 615600:
+						dur = durations - 615600
+				elif d >= 27 and dt == 28 or dt == 29:
+					if durations < 2376000:
+						dur = 2376000 - durations
+					elif durations > 2462400 :
+						dur = durations - 2462400 
+				elif d >= 55 and dt == 56 or dt == 57:
+					if durations < 4752000:
+						dur = 4752000 - durations
+					elif durations > 4924800:
+						dur = durations - 4924800
+				else:
+					dur = durations
 
-			days = dur / SECONDS_PER_DAY
-			dur = dur % SECONDS_PER_DAY
+				days = dur / SECONDS_PER_DAY
+				dur = dur % SECONDS_PER_DAY
 
-			hours = dur / SECONDS_PER_HOUR
-			dur = dur % SECONDS_PER_HOUR
+				hours = dur / SECONDS_PER_HOUR
+				dur = dur % SECONDS_PER_HOUR
 
-			minutes = dur / SECONDS_PER_MINUTE
-			dur = dur % SECONDS_PER_MINUTE
+				minutes = dur / SECONDS_PER_MINUTE
+				dur = dur % SECONDS_PER_MINUTE
 			
-			d = int(dt)
-			#d = int(days)
-			h = int(hours)
-			m = int(minutes)
+				d = int(dt)
+				#d = int(days)
+				h = int(hours)
+				m = int(minutes)
 				
-			if daywithintolerance == True:
-				day = str(day)
-			elif h == 0 and m == 0:
-				day=str(d)
-			elif h == 0:
-				day=str(d)+'     Mn:'+str(m)
-			elif m == 0:
-				day=str(d)+'     Hr:'+str(h)
-			else:
-				day=str(d)+'     Hr:'+str(h)+' Mn:'+str(m)
-				
-	return str(day), str(day), break_message, daywithintolerance
+				if daywithintolerance == True:
+					day = str(day)
+				elif h == 0 and m == 0:
+					day=str(d)
+				elif h == 0:
+					day=str(d)+'     Mn:'+str(m)
+				elif m == 0:
+					day=str(d)+'     Hr:'+str(h)
+				else:
+					day=str(d)+'     Hr:'+str(h)+' Mn:'+str(m)
+
+	return day, day, break_message, daywithintolerance
